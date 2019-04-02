bb66a43d07ecdeaec2c608e4edda55984e787b8b
souvikdgreat <souvikc40@gmail.com>
2019-04-02 10:34:40 +0530

    first commit

---
+++ src/main/java/com/Reap/ReapProject/ReapProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Reap.ReapProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReapProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReapProjectApplication.class, args);
+	}
+
+}
