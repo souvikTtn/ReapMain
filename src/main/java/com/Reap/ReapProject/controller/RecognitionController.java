483e7e507e14e125a87ebc27c2d105d1681c81bf
souvikdgreat <souvikc40@gmail.com>
2019-04-04 12:36:29 +0530

    added bootstraping data

---
+++ src/main/java/com/Reap/ReapProject/controller/RecognitionController.java
@@ -0,0 +1,16 @@
+package com.Reap.ReapProject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RecognitionController {
+
+    /*@PostMapping("/recognizeNewer")
+    public String recognizeUser(Model model){
+
+
+    }*/
+}
--- src/main/java/com/Reap/ReapProject/event/UserBootstrap.java
+++ src/main/java/com/Reap/ReapProject/event/UserBootstrap.java
@@ -36,6 +36,38 @@ public class UserBootstrap {
             user.setRoleSet(roles);
             System.out.println(user+" initilaized");
             userService.addUser(user);
+
+            User user1 = new User();
+            user1.setActive(true);
+            user1.setEmail("deepika.tiwari@tothenew.com");
+            user1.setFirstName("Deepika");
+            user1.setLastName("Tiwari");
+            user1.setPassword("deesssooo");
+            userService.addUser(user1);
+
+            User user3 = new User();
+            user3.setActive(true);
+            user3.setEmail("divya.arora@tothenew.com");
+            user3.setFirstName("Divya");
+            user3.setLastName("Arora");
+            user3.setPassword("deesssooo");
+            userService.addUser(user3);
+
+            User user2 = new User();
+            user2.setActive(true);
+            user2.setEmail("dhruv.oberoi@tothenew.com");
+            user2.setFirstName("Dhruv");
+            user2.setLastName("Oberoi");
+            user2.setPassword("deesssooo");
+            userService.addUser(user2);
+
+            User user4 = new User();
+            user4.setActive(true);
+            user4.setEmail("kanchan.sinha@tothenew.com");
+            user4.setFirstName("Kanchan");
+            user4.setLastName("Sinha");
+            user4.setPassword("deesssooo");
+            userService.addUser(user4);
         }
     }
 }
