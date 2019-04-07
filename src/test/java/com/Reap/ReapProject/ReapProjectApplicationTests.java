4cdffb63055b9d8e6207b5490d46096012558d66
souvikdgreat <souvikc40@gmail.com>
2019-04-07 20:44:14 +0530

    added canary and get test for user/{id}

--- src/test/java/com/Reap/ReapProject/ReapProjectApplicationTests.java
+++ src/test/java/com/Reap/ReapProject/ReapProjectApplicationTests.java
@@ -1,5 +1,7 @@
 package com.Reap.ReapProject;
 
+import com.jayway.restassured.RestAssured;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,5 +14,31 @@ public class ReapProjectApplicationTests {
 	@Test
 	public void contextLoads() {
 	}
+
+	@Test
+	public  void canary(){
+		Assert.assertEquals(1,1);
+	}
+
+	@Test
+	public void testingGoogle(){
+		RestAssured.given().get("https://www.google.com/").then().statusCode(200);
+	}
+	String uri="users/{id}";
+
+	@Test
+	public void validPostTest() {
+		RestAssured.given().pathParam("id",1)
+				.get(uri).then()
+				.statusCode(200).log().all();
+	}
+
+	@Test
+	public void invalidPostTest() {
+		RestAssured.given().pathParam("id",1000)
+				.get(uri).then()
+				.statusCode(200).log().all();
+	}
+
 
 }
